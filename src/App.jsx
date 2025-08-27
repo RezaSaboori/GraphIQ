7dda18409162aa68a36262cc3884fa47dad0dfa2
Reza Saboori Amleshi <reza.saboori.amleshi@gmail.com>
2025-08-27 20:35:40 +0330

    feat: add TubelightEffect component and integrate with ArrowLayer
    - Implement TubelightEffect.jsx for animated arrow highlighting
    - Update ArrowLayer.jsx to support tubelight effect on connections
    - No breaking changes to App.jsx; visual enhancement only

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React, { useCallback } from 'react';
+import { Xwrapper } from 'react-xarrows';
 import GlassCard from './components/GlassCard.jsx';
 import ArrowLayer from './components/ArrowLayer.jsx';
 import useDraggable from './hooks/useDraggable.js';
 
 const connections = [
-  { from: 'card2', to: 'card1', label: '_Related', color: '#facc15' },
-  { from: 'card1', to: 'card3', label: '_Connected', color: '#818cf8' },
-  { from: 'card2', to: 'card3', label: '_Related', color: '#facc15' },
+  { from: 'card2', to: 'card1', label: '_Related', color: '#facc15', width: 4 },
+  { from: 'card1', to: 'card3', label: '_Connected', color: '#818cf8', width: 4 },
+  { from: 'card2', to: 'card3', label: '_Related', color: '#facc15', width: 4 },
 ];
 
 function App() {
@@ -17,42 +18,44 @@ function App() {
 
   return (
     <div className="w-screen h-screen flex items-center justify-center relative">
-      <ArrowLayer connections={connections} />
+      <Xwrapper>
+        <ArrowLayer connections={connections} />
 
-      <GlassCard id="card1" width={400} style={{ top: '45%', left: '15%' }} {...bindDrag}>
-        <div className="label-container">
-          <div className="label label-green">personal history</div>
-          <div className="label label-blue">risk factor</div>
-          <div className="label label-red">Unspecified</div>
-        </div>
-        <div className="property">
-          <span className="property-name">name :</span> Problems associated with the natural environment or human-made changes to the environment
-        </div>
-      </GlassCard>
+        <GlassCard id="card1" width={400} style={{ top: '45%', left: '15%' }} {...bindDrag}>
+          <div className="label-container">
+            <div className="label label-green">personal history</div>
+            <div className="label label-blue">risk factor</div>
+            <div className="label label-red">Unspecified</div>
+          </div>
+          <div className="property">
+            <span className="property-name">name :</span> Problems associated with the natural environment or human-made changes to the environment
+          </div>
+        </GlassCard>
 
-      <GlassCard id="card2" width={400} style={{ top: '15%', left: '55%' }} {...bindDrag}>
-        <div className="label-container">
-          <div className="label label-green">personal history</div>
-          <div className="label label-blue">risk factor</div>
-          <div className="label label-red">Unspecified</div>
-        </div>
-        <div className="property">
-          <span className="property-name">name :</span> Problems associated with the natural environment or human-made changes to the environment
-        </div>
-      </GlassCard>
+        <GlassCard id="card2" width={400} style={{ top: '15%', left: '55%' }} {...bindDrag}>
+          <div className="label-container">
+            <div className="label label-green">personal history</div>
+            <div className="label label-blue">risk factor</div>
+            <div className="label label-red">Unspecified</div>
+          </div>
+          <div className="property">
+            <span className="property-name">name :</span> Problems associated with the natural environment or human-made changes to the environment
+          </div>
+        </GlassCard>
 
-      <GlassCard id="card3" width={400} style={{ top: '55%', left: '60%' }} {...bindDrag}>
-        <div className="label-container">
-          <div className="label label-orange">family history</div>
-          <div className="label label-red">Unspecified</div>
-        </div>
-        <div className="property">
-          <span className="property-name">name :</span> Family history of diseases of the blood or blood-forming organs
-        </div>
-        <div className="property">
-          <span className="property-name">exclusions :</span> Occupational exposure to risk-factors
-        </div>
-      </GlassCard>
+        <GlassCard id="card3" width={400} style={{ top: '55%', left: '60%' }} {...bindDrag}>
+          <div className="label-container">
+            <div className="label label-orange">family history</div>
+            <div className="label label-red">Unspecified</div>
+          </div>
+          <div className="property">
+            <span className="property-name">name :</span> Family history of diseases of the blood or blood-forming organs
+          </div>
+          <div className="property">
+            <span className="property-name">exclusions :</span> Occupational exposure to risk-factors
+          </div>
+        </GlassCard>
+      </Xwrapper>
     </div>
   );
 }
